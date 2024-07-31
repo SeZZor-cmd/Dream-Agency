f562d6dd7f1d46b9d11caa91785c4af60e65fc4d
SeZZor-cmd <das.akash0504@gmail.com>
2024-07-31 16:19:37 +0530

    fourth

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
           Dream Agency
         </h1>
         <p className="text-sm leading-snug mt-5">
-          Lorem Ipsum has been the industry's standard dummy text ever since the
+          Lorem Ipsum has been the industry&apos;s standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
           to make a type specimen book. It has survived not only five centuries.
         </p>
